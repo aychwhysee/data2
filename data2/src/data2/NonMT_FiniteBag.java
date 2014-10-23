6716ddc878eb3478f1514e6272ae02601e061310
Harry Cho <hacho@vassar.edu>
2014-10-23 03:23:04 -0400

    Just thinking out loud in comments
    Trying to wrap my head around multisets

--- data2/src/data2/NonMT_FiniteBag.java
+++ data2/src/data2/NonMT_FiniteBag.java
@@ -5,7 +5,7 @@ public class NonMT_FiniteBag<D> implements FiniteBag {
     D root;
     FiniteBag left;
     FiniteBag right;
-//    int count;
+    int count;
     
     NonMT_FiniteBag(D root) {
         this.root = root;
@@ -21,18 +21,33 @@ public class NonMT_FiniteBag<D> implements FiniteBag {
     }
     
     public int cardinality() {
+        // Can't just do left + right + 1 now, since an elt could be repeated
+        // and can have a count of more than just 1.
+        // So we need a "count" variable
+        // Does this also mean we need a method to return how many times
+        // an elt of type D is in the FiniteBag?
         
     }
     
     public boolean isEmptyHuh() {
+        // NonMT bag is NonMT. Return false.
         return false;
     }
     
     public boolean member(D elt) {
+        // Can work similarly to FiniteSets right? Since it's only checking
+        // if an elt of type D is in the bag, and not how many of it. 
+        // Also, why/how would we get the intersection, subset etc. of
+        // multisets in the first place? With FiniteSets, we used
+        // member as a big player in getting those, but here...
+        // We need to account for # of times an elt appears
         
     }
     
     public FiniteBag add(D elt) {
+        // find a place to add elt into. If elt is = a root, just
+        // add 1 to the count of the elt and return the tree with the new count
+        // else keep checking left and right...uh...and balance yourself, tree!
         
     }
     
@@ -57,7 +72,7 @@ public class NonMT_FiniteBag<D> implements FiniteBag {
     }
     
     public boolean equal(FiniteBag u) {
-        
+        return this.subset(u) && u.subset(this);
     }
     
     public boolean subset(FiniteBag u) {
