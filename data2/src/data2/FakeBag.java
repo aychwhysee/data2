5949b5e0996ed7f5e25928ac0bfae80f3f816ab4
Harry Cho <hacho@vassar.edu>
2014-10-30 17:23:45 -0400

    Notes/planning. Confirm/help?

--- data2/src/data2/FakeBag.java
+++ data2/src/data2/FakeBag.java
@@ -4,4 +4,12 @@ public interface FakeBag<D extends Comparable> {
     
     // Is an interface necessary just for FakeBagL and FakeBag3?
     
+    /* 
+    random notes not really specific to FakeBag; just organizing thoughts:
+    - method smartAdd will call smartAddStep2 which takes in the result of
+      smartAddStep1
+    - private methods is some class(?????) fake1 and fake2 will do magic
+    - So where do we put all the above?
+    */
+    
 }
