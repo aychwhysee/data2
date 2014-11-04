ce82b8db81014d92d1b22d28613ba128ee9711b4
Harry Cho <hacho@vassar.edu>
2014-11-03 20:21:43 -0500

    Cat_Sequence

--- data2/src/data2/Cat_Sequence.java
+++ data2/src/data2/Cat_Sequence.java
@@ -1,15 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package data2;
 
-/**
- *
- * @author Harry
- */
-public class Cat_Sequence {
+public class Cat_Sequence<D extends Comparable> implements Sequence<D> {
     
+    Sequence<D> left;
+    Sequence<D> right;
+    
+    Cat_Sequence(Sequence<D> left, Sequence<D> right) {
+        this.left = left;
+        this.right = right;
+    }
+    
+    public D here() {
+        if (this.left.hasNext()) {
+            return this.left.here();
+        } else {
+            return this.right.here();
+        }
+    }
+    
+    public boolean hasNext() {
+        return this.left.hasNext() || this.right.hasNext();
+    }
+    
+    public Sequence<D> next() {
+        if (this.left.hasNext()) {
+            return new Cat_Sequence(this.left.next(), this.right);
+        } else {
+            return this.right.next();
+        }
+    }
+    
+    public String toStringS() {
+        return this.left.toStringS() + " " + this.right.toStringS();
+    }
 }
